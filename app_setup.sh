4f8042fda8ea35fe6343e54150de097a9ceb236f
Aetina-Tim <92358191+Aetina-Tim@users.noreply.github.com>
2023-11-03 11:18:56 +0800

    Update app_setup.sh
    feat : fix lib install bug

--- app_setup.sh
+++ app_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt-get install -y python3-pip
+pip3 install launchpadlib==1.10.13
+pip3 install setuptools==59.5.0
 pip3 install grpcio
 pip3 install grpcio-tools
 pip3 install tritonclient[all]
